3013204a65013c32479bdc2d54a13fd00343e322
Sabri <saabb.tod@gmail.com>
2022-04-04 00:56:34 +0200

    Reordenacion

--- src/t6/Colecciones/Hotel/Hotel.java
+++ src/t6/Colecciones/Hotel/Hotel.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package t6.RepasoExamen;
+package t6.Colecciones.Hotel;
 
-import t6.RepasoExamen.Zonas;
+import t6.Colecciones.Hotel.Zonas;
 
 /**
  *
