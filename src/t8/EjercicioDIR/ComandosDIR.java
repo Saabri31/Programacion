bcda8b017cc3238783364d9ca39629eaab3350ac
Sabri <saabb.tod@gmail.com>
2022-04-04 12:33:02 +0200

    Ejercicio DIR ara si

--- src/t8/EjercicioDIR/ComandosDIR.java
+++ src/t8/EjercicioDIR/ComandosDIR.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package t8.Ejemplos.EjercicioDIR;
+package t8.EjercicioDIR;
 
 import java.io.File;
 import java.util.Scanner;
