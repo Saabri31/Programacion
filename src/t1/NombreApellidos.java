c5483f646aa7c487b2ea893cf934e51259678871
Sabri <saabb.tod@gmail.com>
2022-04-05 01:11:23 +0200

    Cambios

--- src/t1/NombreApellidos.java
+++ src/t1/NombreApellidos.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package me.sabrina.tema1;
+package t1;
 
 import java.io.Console;
 
